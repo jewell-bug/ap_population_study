54aacabab44ab73e544e4607663442ce60983f26
jewell-bug <jjung@mantis-sub-5.cam.uchc.edu>
2026-05-02 17:58:28 -0400

    add script

--- scripts/05_variantCalling/02_summarize_var.sh
+++ scripts/05_variantCalling/02_summarize_var.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
-#SBATCH --job-name=summary
-#SBATCH -n 1
-#SBATCH -N 1
 #SBATCH -c 1
 #SBATCH --mem=10G
 #SBATCH --qos=general
 #SBATCH --partition=general
-
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
-
 
 module load bcftools
 
@@ -17,7 +13,9 @@ INDIR=../../results/05_variantCalling/bcftools/raw.vcf.gz
 OUTDIR=../../results/05_variantCalling/bcftools_summary
 
 mkdir -p "$OUTDIR"
+
 # Generate variant statistics from VCF
 bcftools stats "$INDIR" | grep "SN" > "$OUTDIR/summary.txt"
+
 # Generate filtered statistics excluding low-quality variants (QUAL < 30)
 bcftools stats -e "QUAL < 30" "$INDIR" | grep "SN" >> "$OUTDIR/summary.txt"
